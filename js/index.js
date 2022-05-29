5f2052f550f9bcda0855ccf4b27634e60d60e088
Minjoo Kim <mkim9313@uw.edu>
2022-05-29 02:20:14 -0700

    complete 7

--- js/index.js
+++ js/index.js
@@ -1,31 +1,47 @@
 //Log out a message to show you've got JavaScript running
-
+console.log("Script running!"); 
 
 
 
 //Define a variable `myName` and log it out
+let myName = "Minjoo Kim";
+console.log(myName);
 
 
 
 
 //Define a function `getVowelCount()`
-
-
+function getVowelCount(aString){
+    lowerCaseString = aString.toLowerCase();
+    modifiedString = lowerCaseString.replaceAll("a","");
+    modifiedString = modifiedString.replaceAll("e","");
+    modifiedString = modifiedString.replaceAll("i","");
+    modifiedString = modifiedString.replaceAll("o","");
+    modifiedString = modifiedString.replaceAll("u","");
+    myString = lowerCaseString.length - modifiedString.length;
+    return(myString);
+}
 
 
 //Define a variable `numVowelsInName` and log it out
 
+let numVowelsInName = getVowelCount(myName);
+console.log(numVowelsInName);
 
 
 
 //Create a variable `h1Elememt` that refers to the `<h1>` element in the DOM.
 //Then change the text of that element
 
-
+let h1Element = $('h1');
+h1Element.text("Interactive Pet Viewer");
 
 
 //Create a variable `footerElement` that refers to the `<footer>`, then
 //change the HTML content of that element
+
+let footerElement = $('footer')
+footerElement.html('<small>All images from <a href="https://unsplash.com/">unsplash.com</a></small>');
 
 
 
@@ -33,8 +49,8 @@
 //Change the CSS `display` property of the `#cats` element
 //Add the `active` class to the `#btnShowDogs` element
 
-
-
+$('#cats').css("display","none");
+$('#btnShowDogs').addClass("active");
 
 //Add an event listener to the buttons to respond to click events.
 //The listener's function will toggle the `#dogs` and `#cats` divs,
